d46bb1e793f07e5aa2c2b113a2a307268f22452e
Rachel <me@rcabot.com>
2016-10-21 14:06:53 +0100

    doh

--- rover/rover/src/main/java/rover/shared/reasoning/APlan.java
+++ rover/rover/src/main/java/rover/shared/reasoning/APlan.java
@@ -2,14 +2,17 @@ package rover.shared.reasoning;
 
 import rover.shared.practical.ARoverAction;
 
-import java.util.ArrayList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by rachelcabot on 17/10/2016.
  */
 public abstract class APlan {
     protected Queue<ARoverAction> actions;
+
+    public APlan(){
+        actions = new LinkedList<>();
+    }
 
     public ARoverAction popStep() {
         return actions.poll();
