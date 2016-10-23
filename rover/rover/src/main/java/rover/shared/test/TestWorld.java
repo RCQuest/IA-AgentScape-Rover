7805e6ebd57c207059b0b9d35568e0442488517b
Rachel <me@rcabot.com>
2016-10-23 14:34:57 +0100

    Have to remove test sources from project

--- rover/rover/src/main/java/rover/shared/test/TestWorld.java
+++
@@ -1,9 +0,0 @@
-package rover.shared.test;
-
-/**
- * Created by rachelcabot on 23/10/2016.
- */
-public class TestWorld {
-    public static double width =16;
-    public static double height =16;
-}
