741f09a8d3d6f9c9574fdf9bdd4eb63af186ac41
Rachel <me@rcabot.com>
2016-11-04 17:41:59 +0000

    do nothing plan now does something

--- rover/rover/src/main/java/rover/reasoning/plans/DoNothingPlan.java
+++ rover/rover/src/main/java/rover/reasoning/plans/DoNothingPlan.java
@@ -1,5 +1,7 @@
 package rover.reasoning.plans;
 
+import rover.shared.actions.MoveBackToBaseAction;
+import rover.shared.actions.MoveTowardsAction;
 import rover.shared.reasoning.ABelief;
 import rover.shared.reasoning.AIntention;
 import rover.shared.reasoning.APlan;
@@ -10,8 +12,13 @@ import java.util.ArrayList;
  * Created by rachelcabot on 21/10/2016.
  */
 public class DoNothingPlan extends APlan {
+
+    public DoNothingPlan(){
+        actions.add(new MoveBackToBaseAction());
+    }
+
     @Override
     public boolean isSound(ArrayList<AIntention> i, ArrayList<ABelief> b) {
-        return false;
+        return true;
     }
 }
