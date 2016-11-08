1940e1560f43cdce5642f62eddf50280c8a9ee43
Rachel <me@rcabot.com>
2016-11-08 16:34:14 +0000

    added scenario optimisations

---
+++ rover/rover/src/main/java/rover/shared/practical/ScenarioOptimisations.java
@@ -0,0 +1,11 @@
+package rover.shared.practical;
+
+import java.util.HashMap;
+
+/**
+ * Created by rachelcabot on 08/11/2016.
+ */
+public class ScenarioOptimisations {
+    private static final int[] scenarioToResourceVolumes = {10,5,5,1,1,2,1,5,15,2};
+    private static final int[] scenarioToResourceNumbers = {1,5,10,10,15,30,10,25,5,50};
+}
